e589e9de61bdeb2c9fd18c4da45759383991047a
israa <israa@gmail.com>
2024-05-11 22:15:38 +0300

    last commit

--- app.js
+++ app.js
@@ -6,4 +6,30 @@ const server = app.listen(PORT, () =>
   console.log(`Chat server on port ${PORT}`)
 );
 
+const io = require("socket.io")(server);
 app.use(express.static(path.join(__dirname, "public")));
+
+let socketsConected = new Set();
+
+io.on("connection", onConnected);
+
+function onConnected(socket) {
+  console.log("Socket connected", socket.id);
+  socketsConected.add(socket.id);
+  io.emit("clients-total", socketsConected.size);
+
+  socket.on("disconnect", () => {
+    console.log("Socket disconnected", socket.id);
+    socketsConected.delete(socket.id);
+    io.emit("clients-total", socketsConected.size);
+  });
+
+  socket.on("message", (data) => {
+    // console.log(data)
+    socket.broadcast.emit("chat-message", data);
+  });
+
+  socket.on("feedback", (data) => {
+    socket.broadcast.emit("feedback", data);
+  });
+}
